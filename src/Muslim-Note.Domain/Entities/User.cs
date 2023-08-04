42b9a5c6d78c08afd81219a2025b8b57b6330c99
Muxammad <muxammadcodes@gmail.com>
2023-08-04 05:34:54 -0700

    reminder

--- src/Muslim-Note.Domain/Entities/User.cs
+++ src/Muslim-Note.Domain/Entities/User.cs
@@ -1,14 +1,13 @@
 ﻿using Muslim_Note.Domain.Commons;
 
-namespace Muslim_Note.Domain.Entities
+namespace Muslim_Note.Domain.Entities;
+
+public class User : Auditable
 {
-    public class User : Auditable
-    {
-        public string FirstName { get; set; } = string.Empty;
-        public string LastName { get; set; } = string.Empty;
-        public string Email { get; set; } = string.Empty;
-        public string PhoneNumber { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
-        public string Salt { get; set; } = string.Empty;
-    }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string PhoneNumber { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public string Salt { get; set; } = string.Empty;
 }
