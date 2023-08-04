81b341b0976d6eaebf3dd842664bf017b7889f30
Muxammad <muxammadcodes@gmail.com>
2023-08-04 05:35:28 -0700

    user

--- src/Muslim-Note.Domain/Entities/User.cs
+++ src/Muslim-Note.Domain/Entities/User.cs
@@ -6,8 +6,8 @@ public class User : Auditable
 {
     public string FirstName { get; set; } = string.Empty;
     public string LastName { get; set; } = string.Empty;
-    public string Email { get; set; } = string.Empty;
     public string PhoneNumber { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
     public string Password { get; set; } = string.Empty;
     public string Salt { get; set; } = string.Empty;
 }
