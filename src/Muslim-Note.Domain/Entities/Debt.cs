9c19f62b29a5dcd32a2aab07a680a7afef0890bc
Muxammad <muxammadcodes@gmail.com>
2023-08-04 05:32:00 -0700

    debt

--- src/Muslim-Note.Domain/Entities/Debt.cs
+++ src/Muslim-Note.Domain/Entities/Debt.cs
@@ -5,7 +5,7 @@ namespace Muslim_Note.Domain.Entities;
 public class Debt : Auditable
 {
     public long UserId { get; set; }
-    public User User { get; set; }
+    public User User { get; set; } = default!;
     public decimal Amount { get; set; }
     public string Lender { get; set; } = string.Empty;
     public DateOnly FundingDate { get; set; }
