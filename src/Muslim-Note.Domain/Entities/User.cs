30f1165e0b2e08220623825cf0a8c0939398b736
Muxammad <muxammadcodes@gmail.com>
2023-08-04 04:52:53 -0700

    user

--- src/Muslim-Note.Domain/Entities/User.cs
+++ src/Muslim-Note.Domain/Entities/User.cs
@@ -1,12 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Muslim_Note.Domain.Commons;
 
 namespace Muslim_Note.Domain.Entities
 {
-    internal class User
+    public class User : Auditable
     {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string Salt { get; set; } = string.Empty;
+
     }
 }
