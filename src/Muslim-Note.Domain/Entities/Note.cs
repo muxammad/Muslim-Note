48b1a87f6e94a6b21f02eb518bd9439c0f9a8232
Muxammad <muxammadcodes@gmail.com>
2023-08-04 05:01:55 -0700

    note upd

--- src/Muslim-Note.Domain/Entities/Note.cs
+++ src/Muslim-Note.Domain/Entities/Note.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Muslim_Note.Domain.Commons;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,10 @@ using System.Threading.Tasks;
 
 namespace Muslim_Note.Domain.Entities
 {
-    public class Note
+    public class Note : Auditable
     {
+        public long  UserId { get; set; }
+        public User User { get; set; }
+        public string NoteText { get; set; } = string.Empty;
     }
 }
