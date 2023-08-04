bd5baae1a33e2be50cedef44e274427295def6c4
Muxammad <muxammadcodes@gmail.com>
2023-08-04 04:19:29 -0700

    Auditable

---
+++ src/Muslim-Note.Domain/Commons/Auditable.cs
@@ -0,0 +1,9 @@
+﻿namespace Muslim_Note.Domain.Commons
+{
+    public abstract class Auditable
+    {
+        public long Id { get; set; }
+        public DateTime CreateAt { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; set;}
+    }
+}
