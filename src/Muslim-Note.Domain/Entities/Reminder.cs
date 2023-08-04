d512a366d9e53bf7b7bba26e091ce9e62e92d713
Muxammad <muxammadcodes@gmail.com>
2023-08-04 05:01:26 -0700

    reminder

--- src/Muslim-Note.Domain/Entities/Reminder.cs
+++ src/Muslim-Note.Domain/Entities/Reminder.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Muslim_Note.Domain.Commons;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Muslim_Note.Domain.Entities
 {
-    internal class Reminder
+    public class Reminder : Auditable
     {
     }
 }
